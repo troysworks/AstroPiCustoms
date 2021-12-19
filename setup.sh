181591b61a9bc020e9318fd5f7f9dfd95c895169
troysworks <troysworks@gmail.com>
2021-12-19 12:45:32 -0700

    Initial setup script

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+APP_NAME="AstroPiCustoms"
+
+echo "Updating apt"
+sudo apt update -y
+sudo apt upgrade -y
+
+echo "installing wget"
+sudo apt install git wget python python3-venv -y
+
+CUR_PATH=${PWD##*/}
+
+if [ $APP_NAME != "$CUR_PATH" ]; then
+  if [ -d $APP_NAME ]; then
+    echo "Cloning git repo"
+    git clone https://github.com/troysworks/AstroPiCustoms.git
+  fi
+  echo "Changing directory"
+  cd $APP_NAME || exit
+fi
+
+if [ -d "venv" ]; then
+  echo "Creating new venv path"
+  rm -rf venv
+fi
+
+echo "Upgrade pip and install venv"
+python3 -m pip install --upgrade pip
+python3 -m venv venv
+
+echo "Activating venv"
+source venv/bin/activate
+
+echo "Update pip"
+python -m pip install --upgrade pip
+
+echo "Installing requirements"
+pip install -r requirements.txt
+
+if [ -d "src" ]; then
+  echo "Cloning Git Repo"
+  rm -rf venv
+fi
+
+echo "Setup complete"
